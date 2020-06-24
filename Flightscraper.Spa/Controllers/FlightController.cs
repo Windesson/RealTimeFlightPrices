f9c3f19c95f3a9bc11119132e8699f9a2652605f
Silva <windesson@hotmail.com>
2020-06-24 18:59:47 -0400

    minor fix

--- Flightscraper.Spa/Controllers/FlightController.cs
+++ Flightscraper.Spa/Controllers/FlightController.cs
@@ -111,12 +111,7 @@ namespace Flightscraper.Spa.Controllers
 
                 if (IsDateWithinAllowedRange(departDate)) // depart cannot be more than 334 days 
                 {
-                    ModelState.AddModelError("outboundDate", $"Depart date cannot be more than than {maxTotalDaysRangeAllowed} days from today's date.");
-                }
-
-                if (IsDateWithinAllowedRange(departDate)) // depart cannot be more than 334 days 
-                {
-                    ModelState.AddModelError("outboundDate", $"Depart date cannot be more than than {maxTotalDaysRangeAllowed} days from today's date.");
+                    ModelState.AddModelError("outboundDate", $"Depart date cannot be more than than {maxTotalDaysRangeAllowed} days from today's date. `{DateTime.Now.ToString("MM/dd/yyyy")}`");
                 }
 
                 if (!returnDateString.IsNullOrEmpty()) // if return date is provided
@@ -130,9 +125,8 @@ namespace Flightscraper.Spa.Controllers
 
                         if (IsDateWithinAllowedRange(returnDate)) // depart cannot be more than 334 days 
                         {
-                            ModelState.AddModelError("inboundDate", $"Return date cannot be more than {maxTotalDaysRangeAllowed} days from today's date.");
+                            ModelState.AddModelError("inboundDate", $"Return date cannot be more than {maxTotalDaysRangeAllowed} days from today's date. `{DateTime.Now.ToString("MM/dd/yyyy")}`");
                         }
-
                     }
                     else
                     {
